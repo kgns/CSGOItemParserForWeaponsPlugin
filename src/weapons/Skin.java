e2c1229e74ea5f774d6ed238bb192902c8b5a1eb
Adam Volkman (itIsMaku) <68100107+itIsMaku@users.noreply.github.com>
2021-11-18 12:55:52 +0100

    feat: lombok imports

--- src/weapons/Skin.java
+++ src/weapons/Skin.java
@@ -3,6 +3,10 @@ package weapons;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+
 @Getter
 @Setter
 @EqualsAndHashCode
